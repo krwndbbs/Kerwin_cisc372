08dd4dd40306100ecd0b9facd4f9d00bfeca0c9f
Kerwin Dobbs <dobbskd@ubuntu-1804.udel.edu>
2019-09-01 11:09:46 -0400

    updating hw1 folder with Kerwin_hello.c

---
+++ hw1/Kerwin_hello.c
@@ -0,0 +1,16 @@
+/*
+ * Kerwin_hello.c
+ *
+ *  Created on: Sep 1, 2019
+ *      Author: dobbskd
+ */
+
+#include <stdio.h>
+
+int main() {
+	int n = 0;
+	while(n < 10) {
+		n++;
+		printf(" Hello Kerwin\n");
+	}
+}
